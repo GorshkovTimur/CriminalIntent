00d2383b3535a46251d9c727f0b3a3c48bea0a85
TimurGorshkov <gorshkoff.timur@gmail.com>
2019-04-28 18:40:51 +0300

    Допиливаем класс Crime + интерфейс

--- app/src/main/java/com/timmyg/criminalintent/Crime.java
+++ app/src/main/java/com/timmyg/criminalintent/Crime.java
@@ -1,25 +1,45 @@
 package com.timmyg.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
     private UUID mId;
-    private String mTitile;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
         return mId;
     }
 
-    public String getTitile() {
-        return mTitile;
+    public String getTitle() {
+        return mTitle;
     }
 
-    public void setTitile(String mTitile) {
-        this.mTitile = mTitile;
+    public void setTitile(String mTitle) {
+        this.mTitle = mTitle;
     }
 }
