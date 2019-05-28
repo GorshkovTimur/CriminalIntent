0d3e89b9b7eb3907f1550bc5a66f834c0a504093
TimurGorshkov <gorshkoff.timur@gmail.com>
2019-05-28 16:04:37 +0300

    Первичная инициализация через БД

--- app/src/main/java/com/timmyg/criminalintent/CrimeLab.java
+++ app/src/main/java/com/timmyg/criminalintent/CrimeLab.java
@@ -1,6 +1,12 @@
 package com.timmyg.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.timmyg.criminalintent.database.CrimeBaseHelper;
+import com.timmyg.criminalintent.database.CrimeDbSchema;
+import com.timmyg.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,14 +15,17 @@ import java.util.UUID;
 public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public void addCrime (Crime c){
-        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.Name, null , values);
+
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>();
     }
 
     public static CrimeLab get (Context context){
@@ -26,17 +35,30 @@ public class CrimeLab {
         return sCrimeLab;
     }
     private CrimeLab(Context context){
-
-        mCrimes = new ArrayList<>();
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
 
     }
 
     public Crime getCrime (UUID id){
-        for (Crime crime : mCrimes ){
-            if (crime.getId().equals(id)){
-                return crime;
-            }
-        }
         return null;
+    }
+
+    public void updateCrime (Crime crime){
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(CrimeTable.Name, values, CrimeTable.Cols.UUID + " = ?", new String[] {uuidString});
+    }
+
+    public static ContentValues getContentValues (Crime crime){
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle().toString());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().toString());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+
+        return values;
+
     }
 }
